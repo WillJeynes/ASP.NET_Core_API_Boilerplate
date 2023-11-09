3cac955bb47f9140ee75b57c1a3097d0e570bf2d
WillJeynes <william.jeynes@hotmail.com>
2023-11-09 22:22:09 +0000

    firce hit reset

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 cd /home/helloworld/api/HackSheffield9
 killall dotnet
-git pull
+git reset --hard origin/master
 curl localhost:2019/load -H "Content-Type: application/json" -d @caddy_config.json
 chmod a+rwx ~/api/HackSheffield9/data.db
 dotnet publish --configuration Release HackSheffield.csproj 
